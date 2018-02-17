662c0f8ef1f5d5560a8755e48bc14002097b2601
Kevin  F. Konrad <kevinkonrad@ymail.com>
2018-02-17 16:27:25 +0100

    Create init.fish

---
+++ init.fish
@@ -0,0 +1,40 @@
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+alias down='sudo shutdown -h now'
+alias fucking=sudo
+alias history='fc -l 1'
+alias l='ls -lapshG'
+alias la='ls -lApsh'
+alias ll='ls -ps1'
+alias md='mkdir -p'
+alias nap='sudo shutdown -s now >/dev/null'
+alias pc='sudo port clean --all installed'
+alias pi='sudo port install '
+alias please=sudo
+alias po=popd
+alias psu='sudo port selfupdate'
+alias pu=pushd
+alias puni='sudo port uninstall inactive'
+alias puo='sudo port upgrade outdated'
+alias pup='psu; and puo'
+alias rd=rmdir
+alias suno='sudo nano'
+alias sysupdate='texupdate;pup; and pc'
+alias t=todotxt
+alias tex-help='ls -1 /Users/Shared/texdir/scripts/'
+alias texupdate='sudo tlmgr update --self --all --reinstall-forcibly-removed'
+alias topo='top -o cpu -stats '\''command,cpu,mem,th,pid,command,csw,time,state,user,command'\'
+alias umountceloplan='umount /Users/wrk/mnt/celoplan'
+alias which-command=whence
+alias xstop='killall kill Xquartz'
+alias          ...='cd ../..'
+alias         ....='cd ../../..'
+alias        .....='cd ../../../..'
+alias       ......='cd ../../../../..'
+alias      .......='cd ../../../../../..'
+alias     ........='cd ../../../../../../..'
+alias    .........='cd ../../../../../../../..'
+alias   ..........='cd ../../../../../../../../..'
+alias  ...........='cd ../../../../../../../../../..'
+alias ............='cd ../../../../../../../../../../..'
