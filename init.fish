cca404f4b88cfb229a43b80bc90a88d4735bdd05
Kevin F. Konrad <kevin.konrad@skillbyte.de>
2024-05-15 10:29:28 +0200

    thin out unused aliases

--- init.fish
+++ init.fish
@@ -9,34 +9,8 @@ alias ll='command ls -Gp'
 function ld
     grep '/$' (ls -dp1 $argv | psub) --colour=never
 end
-alias md='mkdir -p'
-alias nap='sudo shutdown -s now >/dev/null'
-function pc
-    echo -n "Cleaning MacPorts..."
-    sudo port -N clean --all installed $argv \
-      | sed "s/--->  Cleaning//" \
-      | tr -ds \n " "
-    echo
-end
-alias pi='sudo port -N install '
 alias please=sudo
-alias po=popd
-alias psu='sudo port -N selfupdate'
-alias pu=pushd
-alias puni='sudo port -N uninstall inactive'
-alias puo='sudo port -N upgrade outdated'
-alias pup='psu; and puo'
-alias rd=rmdir
-alias suno='sudo nano'
-alias t=todotxt
 alias texupdate='sudo (which tlmgr) update --self --all --reinstall-forcibly-removed'
-alias portupdate='pup; pc'
-alias brewupdate='brew update; and brew upgrade; and brew cask upgrade'
-alias npmupdate='sudo npm i -g npm'
-alias sysupdate='texupdate; npmupdate; brewupdate; portupdate'
-alias topo='top -o cpu -stats '\''command,cpu,mem,th,pid,command,csw,time,state,user,command'\'
-alias which-command=whence
-alias xstop='killall kill Xquartz'
 alias          ...='cd ../..'
 alias         ....='cd ../../..'
 alias        .....='cd ../../../..'
